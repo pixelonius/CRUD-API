d2ced18ffe8402f910d8e2f3190e514242120772
Plastoi Razvan <razvanplastoi@yahoo.com>
2023-04-08 16:55:23 +0300

    Things are happening...

---
+++ models/contact.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const contactSchema = new mongoose.Schema({
+  name: {
+  type: String,
+  required: true
+  },
+  email:{
+    type: String,
+    required: true
+  },
+  phone:{
+    type: String,
+    required: true
+  },
+  address:{
+    type: String
+  },
+  notes:{
+    type:String
+  },
+
+})
+
+module.exports=mongoose.model('Contact', contactSchema)
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,45 @@
 const express=require('express')
 const router = express.Router()
+const Contact=require('../models/contact')
 
 //get all
+router.get('/',async (req,res)=>{
+try{
+const contacts=await Contact.find()
+res.json(contacts)
+} catch(err){
+  res.status(500).json({message:err.message})
+}
+})
 //get one
+router.get('/:id',(req,res)=>{
+  res.send(req.params.id)
+})
 //create
+router.post('/', async (req,res)=>{
+  const contact=new Contact({
+    name:req.body.name,
+    email:req.body.email,
+    phone:req.body.phone,
+    address:req.body.address,
+    notes:req.body.notes,
+
+  })
+
+  try{
+    const newContact=await contact.save()
+    res.status(201).json(newContact)
+  }catch(err){
+    res.status(400).json({message:err.message})
+  }
+})
 //update
+router.put('/',(req,res)=>{
+  
+})
 //delete
+router.delete('/:id',(req,res)=>{
+  
+})
 
 module.exports=router
